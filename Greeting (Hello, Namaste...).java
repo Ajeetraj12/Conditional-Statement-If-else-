578246c6d02eb8344b3fedb0984e479086ac91d8
Ajeet raj <109308346+Ajeetraj12@users.noreply.github.com>
2023-03-22 15:57:12 +0530

    Update Greeting (Hello, Namaste...).java

--- Greeting (Hello, Namaste...).java
+++ Greeting (Hello, Namaste...).java
@@ -1,5 +1,5 @@
 import java.util.*;
-class scanner {
+public class scanner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int button = sc.nextInt();
